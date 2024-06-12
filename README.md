2a7e456979f33ae9c4dad5b826a9e076811009b4
wmmando0 <59392618+waelmando0@users.noreply.github.com>
2024-06-12 17:31:05 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+* pnpm create-next-app@rc --turbo
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "../components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="">
+      <div className="max-w-6xl mx-auto px-4 py-4">
+        <p>Home</p>
+        <Button>Click</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
